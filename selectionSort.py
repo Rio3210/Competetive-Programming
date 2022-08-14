1060610a8bae43f58813785313ad9932a28c3151
Rio3210 <101450610+Rio3210@users.noreply.github.com>
2022-08-15 00:25:00 +0300

    Create selectionSort.py

---
+++ selectionSort.py
@@ -0,0 +1,20 @@
+class Solution:
+    def selectionSort(self,arr,n):
+        for i in range(n - 1): 
+            min_index = i 
+            for j in range(i + 1, n):
+                if arr[j] < arr[min_index]: 
+                    min_index= j
+    
+            if min_index != i: 
+                arr[i], arr[min_index] = arr [min_index], arr[i]
+
+if __name__ == "__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=list(map(int,input().strip().split()))
+        Solution().selectionSort(arr,n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
