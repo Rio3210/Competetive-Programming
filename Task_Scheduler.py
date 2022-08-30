e0ad168c03cf14e54b56644a03129c46a3743e71
Surafel <surafelwork32@gmail.com>
2022-08-30 20:37:59 +0300

    Create Task_Scheduler.py

---
+++ Task_Scheduler.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        count=[]
+        for value in Counter(tasks).values():
+            count.append(value)
+        count.sort()
+        max_freq=count.pop()
+        room=max_freq-1
+        idle=(room)*n
+        while count and idle > 0:
+            idle -= min(room, count.pop())
+        return max(0,idle)+len(tasks)
