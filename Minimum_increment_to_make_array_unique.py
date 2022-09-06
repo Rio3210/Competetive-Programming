662da32518ccef4bc15c2ece833d3cdd15633f73
Surafel <surafelwork32@gmail.com>
2022-09-07 02:25:19 +0300

    Create Minimum_increment_to_make_array_unique.py

---
+++ Minimum_increment_to_make_array_unique.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minIncrementForUnique(self, nums):
+        nums.sort()
+        c=0
+        i=1
+        num=[]
+        
+        while i<len(nums):
+            if nums[i]<=nums[i-1]:
+                a=nums[i-1]+1
+                c+=(a-nums[i])
+                nums[i]=a
+            i+=1
+        return c
