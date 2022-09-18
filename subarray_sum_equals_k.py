3ea186ac85e6aebd50e5f766a3af0f45871f7b72
Surafel <surafelwork32@gmail.com>
2022-09-18 22:39:59 +0300

    Create subarray_sum_equals_k.py

---
+++ subarray_sum_equals_k.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        
+		ans=0
+		prefsum=0
+		d={0:1}
+
+		for num in nums:
+			prefsum = prefsum + num
+
+			if prefsum-k in d:
+				ans = ans + d[prefsum-k]
+
+			if prefsum not in d:
+				d[prefsum] = 1
+			else:
+				d[prefsum] = d[prefsum]+1
+
+		return ans
+                    
+                
