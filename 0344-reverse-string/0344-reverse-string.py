1dcc85831c1deffa96f2123e81c7ea6f08cdf931
Surafel Workayehu <surafelwork32@gmail.com>
2023-03-13 22:46:09 +0300

    Time: 194 ms (86.39%), Space: 43 MB (5.03%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,12 +1,17 @@
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
+class Solution(object):
+    def reverseString(self, s:List[str])->None:
+        def checker(l,r,string):
+            if l>=r:
+                return 
+            string[l],string[r]=string[r],string[l]
+            l+=1
+            r-=1
+            checker(l,r,string)
+        return checker(0,len(s)-1,s)
         
-        a=len(s)//2
-        b=len(s)-1
-        for i in range(a):
-            s[i],s[b]=s[b],s[i]
-            b-=1
-        """
-        Do not return anything, modify s in-place instead.
-        """
+        # a=int(len(s)/2)
+        # b=len(s)-1
+        # for i in range(0,a):
+        #     s[i],s[b]=s[b],s[i]
+        #     b-=1
         
