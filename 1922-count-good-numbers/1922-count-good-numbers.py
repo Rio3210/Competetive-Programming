c65f2e7f56c1c078e1e9c6a7fb2682a18f610c3d
Surafel Workayehu <surafelwork32@gmail.com>
2023-03-17 13:31:47 +0300

    Time: 36 ms (46.33%), Space: 13.9 MB (10.22%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -15,3 +15,17 @@ class Solution:
         odd = n//2
         return pow(5, evn)*pow(4, odd)%MOD
         
+        
+#         MOD = 10**9 + 7
+#         if n==1:
+#             return 5
+    
+#         def count(i: int) -> int:
+#             if i == n:
+#                 return 1
+#             if i%2==0:
+#                 return (4 * count(i + 1)) % MOD
+#             else:
+#                 return (5 * count(i + 1)) % MOD
+
+#         return count(0)
