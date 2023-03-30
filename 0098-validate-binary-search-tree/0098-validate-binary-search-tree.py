6f989b9b00a3a132e974a3f8ed7fd7f9e0f70f43
Surafel Workayehu <surafelwork32@gmail.com>
2023-03-30 15:54:03 +0300

    Time: 49 ms (44.93%), Space: 17 MB (12.13%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,11 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def helper(root,left,right):
-            if not root:
-                return True
-            if not (root.val>left and root.val<right):
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:  
+        def in_order_traversal(node):
+            if node is None:
+                return []
+            return (in_order_traversal(node.left) 
+            + [node.val] + in_order_traversal(node.right))
+        
+        nodes = in_order_traversal(root)
+        # print(nodes)
+        for i in range(1, len(nodes)):
+            if nodes[i-1] >= nodes[i]:
                 return False
-            return helper(root.left,left,root.val)and helper(root.right,root.val,right)
-        return helper(root,-inf,inf)
+        return True
+    
+    
+    
+        # def helper(root,left,right):
+        #     if not root:
+        #         return True
+        #     if not (root.val>left and root.val<right):
+        #         return False
+        #     return helper(root.left,left,root.val)and helper(root.right,root.val,right)
+        # return helper(root,-inf,inf)
