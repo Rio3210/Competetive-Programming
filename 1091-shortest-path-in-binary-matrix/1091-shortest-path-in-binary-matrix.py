76d21a8fd39ee9644c9989cdef8a552cd982b056
Surafel Workayehu <surafelwork32@gmail.com>
2023-05-24 21:58:30 +0300

    Time: 640 ms (56.78%), Space: 17.7 MB (27.82%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,12 +5,12 @@ class Solution:
             return -1
         queue = deque([(0, 0, 1)]) 
         visited = set([(0, 0)])
-
+        directions=[(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
         while queue:
             x, y, dist = queue.popleft()
             if x == n-1 and y == n-1:
                 return dist
-            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+            for dx, dy in directions:
                 nx, ny = x+dx, y+dy
                 if nx < 0 or nx >= n or ny < 0 or ny >= n or grid[nx][ny] == 1 or (nx, ny) in visited:
                     continue
