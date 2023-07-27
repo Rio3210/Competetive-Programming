a33a3dec8005d2fe1559652a271ab04917d110f6
Surafel Workayehu <surafelwork32@gmail.com>
2023-07-27 22:03:22 +0300

    Time: 34 ms (98.82%), Space: 16.2 MB (74.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo={}
-        n=len(nums)
-        def helper(i):
-            if i==n-1:
-                return nums[n-1]
-            if i==n-2:
-                return max(nums[n-1],nums[n-2])
-            if i in memo:
-                return memo[i]
-            result=max(helper(i+1),helper(i+2) + nums[i])
-            memo[i]=result
-            return result
-        return helper(0)
+        # memo={}
+        # n=len(nums)
+        # def helper(i):
+        #     if i==n-1:
+        #         return nums[n-1]
+        #     if i==n-2:
+        #         return max(nums[n-1],nums[n-2])
+        #     if i in memo:
+        #         return memo[i]
+        #     result=max(helper(i+1),helper(i+2) + nums[i])
+        #     memo[i]=result
+        #     return result
+        # return helper(0)
+        summ1,summ2=0,0
+        for i in nums:
+            temp=max(summ1+i,summ2)
+            summ1=summ2
+            summ2=temp
+        return summ2
