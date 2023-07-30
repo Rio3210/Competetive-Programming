20ce32e7488675ee094297510804888c170cfd7a
Surafel Workayehu <surafelwork32@gmail.com>
2023-07-31 00:32:04 +0300

    Time: 17 ms (97.62%), Space: 13.1 MB (93.12%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        
+        
+        i = len(nums)-2
+        while i >= 0 and nums[i+1] <= nums[i]:
+            i -= 1
+        
+        # If any such index is found then swap its value with the first greater element from right.
+        if i >= 0:
+            j = len(nums)-1
+            while j >= 0 and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        # Reverse the further array items
+        i += 1
+        j = len(nums)-1
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
