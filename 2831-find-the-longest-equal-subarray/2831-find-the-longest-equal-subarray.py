125b7845335cc3b019d3a08d0219de8402f2b3cc
Surafel Workayehu <surafelwork32@gmail.com>
2023-08-29 21:35:49 +0300

    Time: 1431 ms (79.88%), Space: 36.5 MB (36.62%) - LeetHub

---
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestEqualSubarray(self, nums: List[int], k: int) -> int:
+        indexMap = {}
+        maxLength = 0
+
+        for i, num in enumerate(nums):
+            if num in indexMap:
+                indexMap[num].append(i)
+            else:
+                indexMap[num] = [i]
+        
+        for indices in indexMap.values():
+            left = 0
+            right = 0
+            while right < len(indices):
+                if (indices[right] - indices[left]) - (right - left) > k:
+                    left += 1
+                maxLength = max(maxLength, right - left + 1)
+                right += 1
+
+        return maxLength
