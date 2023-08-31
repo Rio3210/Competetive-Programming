8f15a5a594ef906f0ecd0a39ea0f912583d0b581
Surafel Workayehu <surafelwork32@gmail.com>
2023-08-31 22:22:04 +0300

    Time: 44 ms (67.44%), Space: 16.3 MB (96.59%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        st=[0]
+        n=len(s)
+        for i in range(n):
+            
+            for j in st:
+                if s[j:i+1] in wordDict:
+                    st.append(i+1)
+                    break
+        if len(s)==st[-1]:
+            return True
+        else:
+            return False
+            
